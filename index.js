9f385cbcbe9a6918bea4a41a8952fccd10b64b69
sandrobraga <sandrobraga@softbox.com.br>
2019-10-31 16:54:38 -0300

    Testes

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const cors    = require('cors')
 const app     = express()
 
-app.use(cors())
+app.use(cors());
 
 app.listen(3000, () => {
     console.log('Listening on app port 3000');
-})
+});
 
 app.get('/', (req, res) => {
     res.send(JSON.stringify({"mensagem":"Hello world"}));
     return;
-})
+});
 
